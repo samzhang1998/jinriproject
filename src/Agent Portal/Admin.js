e982600d8f04a7933843d4d14f071e3f41c07f1d
sam <sam@starx.com.au>
2024-11-18 15:01:33 +0800

    modify

--- src/Agent Portal/Admin.js
+++ src/Agent Portal/Admin.js
@@ -1,12 +1,7 @@
 import React, { useState } from "react";
 import { Link,useParams } from "react-router-dom";
-import UserSettings from "./UserSettings";
-import add from '../asset/File_dock_add_fill.png';
-import add1 from '../asset/File_dock_add_fill (1).png';
 import fill from '../asset/File_dock_fill.png';
 import fill1 from '../asset/File_dock_fill (1).png';
-import set from '../asset/Setting_fill.png';
-import set1 from '../asset/Setting_fill (1).png';
 import logout from '../asset/Sign_in_squre_fill.png';
 import Header from "../Header";
 
@@ -26,10 +21,10 @@ const CustomerStatus = () => {
     );
 };
 
-const Settings = () => {
+const PropertyStatus = () => {
     return (
         <div>
-            <UserSettings />
+            <h1>Property</h1>
         </div>
     );
 };
@@ -61,8 +56,8 @@ const Admin = () => {
                             background: currentPage === 1 ? "#F4F4F4" : "#FFF",
                         }}
                     >
-                        {currentPage === 1 && <img src={add} alt="add" />}
-                        {currentPage !== 1 && <img src={add1} alt="add1" />}
+                        {currentPage === 1 && <img src={fill1} alt="fill1" />}
+                        {currentPage !== 1 && <img src={fill} alt="fill" />}
                         <p
                             style={{
                                 color: currentPage === 1 ? "#008286" : "#A4A4A4",
@@ -82,7 +77,7 @@ const Admin = () => {
                             style={{
                                 color: currentPage === 2 ? "#008286" : "#A4A4A4",
                             }}
-                        >Customers</p>
+                        >Customer Status</p>
                     </div>
                     <div 
                         onClick={showSettings} 
@@ -91,13 +86,13 @@ const Admin = () => {
                             background: currentPage === 3 ? "#F4F4F4" : "#FFF",
                         }}
                     >
-                        {currentPage === 3 && <img src={set1} alt="set1" />}
-                        {currentPage !== 3 && <img src={set} alt="set" />}
+                        {currentPage === 3 && <img src={fill1} alt="fill1" />}
+                        {currentPage !== 3 && <img src={fill} alt="fill" />}
                         <p
                             style={{
                                 color: currentPage === 3 ? "#008286" : "#A4A4A4",
                             }}
-                        >Account Setting</p>
+                        >Property Status</p>
                     </div>
                     <Link to={"/"} style={{textDecoration: 'none'}}>
                         <div className="to_details">
@@ -109,7 +104,7 @@ const Admin = () => {
                 <div className="portal_details">
                     {currentPage === 1 && <OrderStatus />}
                     {currentPage === 2 && <CustomerStatus />}
-                    {currentPage === 3 && <Settings />}
+                    {currentPage === 3 && <PropertyStatus />}
                 </div>
             </div>
         </div>
