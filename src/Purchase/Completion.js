3ef4bad265b4107ad020c2c011631282c6373653
XiaoHu <oliver@starx.com.au>
2024-12-03 14:04:45 +1100

    payment update

--- src/Purchase/Completion.js
+++ src/Purchase/Completion.js
@@ -3,11 +3,39 @@ import { Link, useNavigate } from 'react-router-dom';
 import Header from '../Header';
 import FetchFunc from '../API';
 import back from "../asset/Expand_left.png";
+import { stripePromise } from './Payment';
 
-function Completion(props) {
+function Completion() {
   const [ messageBody, setMessageBody ] = useState('');
   const navigate = useNavigate();
-  const { stripePromise } = props;
+  const [status, setStatus] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  // const { stripePromise } = props;
+  // const stripePromise = loadStripe("pk_test_51QPcunABwpEZeajXo2pch6nBD11Mj3NckAnEKJIXBa6GVNbByataVG6CuGnlRlfWmoOt9RpHatjU47piXri1d1O700u6TfquCT");
+
+  const submitForm = async () => {
+    const formPurchase = sessionStorage.getItem("formPurchase");
+    if (!formPurchase) {
+      // console.error("No formPurchase data found in sessionStorage.");
+      return; // Exit the function if the data is missing
+    }
+    try {
+      const response = await FetchFunc(
+        '/customer-order/create',
+        'POST',
+        formPurchase
+      );
+
+      if (!response.ok) {
+        console.log(await response.text()); // Wait for the response text
+      } else {
+        console.log('Response from server:', await response.json()); // Process response
+        sessionStorage.removeItem("formPurchase");
+      }
+    } catch (error) {
+      console.error('Error submitting form:', error);
+    }
+  };
 
   useEffect(() => {
     if (!stripePromise) return;
@@ -17,40 +45,59 @@ function Completion(props) {
       const clientSecret = url.searchParams.get('payment_intent_client_secret');
       const { error, paymentIntent } = await stripe.retrievePaymentIntent(clientSecret);
 
-      setMessageBody(error ? `> ${error.message}` : (
-        <>&gt; Payment {paymentIntent.status}: <a href={`https://dashboard.stripe.com/test/payments/${paymentIntent.id}`} target="_blank" rel="noreferrer">{paymentIntent.id}</a></>
-      ));
+      
+      if (error) {
+        // setMessageBody(`> ${error.message}`);
+        setErrorMsg(`> ${error.message}`);
+      } else if (paymentIntent) {
+        setMessageBody((
+          <>&gt; Payment {paymentIntent.status}: <a href={`https://dashboard.stripe.com/test/payments/${paymentIntent.id}`} target="_blank" rel="noreferrer">{paymentIntent.id}</a></>
+        ));
+        // submitForm();
+        setStatus(paymentIntent.status);
+      }
+      
+      
     });
-  }, [stripePromise]);
+    
+  }, []);
   
   useEffect(() => {
-    const submitForm = async () => {
-      const formPurchase = sessionStorage.getItem("formPurchase");
-      if (!formPurchase) {
-        // console.error("No formPurchase data found in sessionStorage.");
-        return; // Exit the function if the data is missing
-      }
-      try {
-        const response = await FetchFunc(
-          '/customer-order/create',
-          'POST',
-          formPurchase
-        );
+    if(status === 'succeeded') {
+      submitForm();
+    }
+    console.log(status);
+  }, status)
   
-        if (!response.ok) {
-          console.log(await response.text()); // Wait for the response text
-        } else {
-          console.log('Response from server:', await response.json()); // Process response
-          sessionStorage.removeItem("formPurchase");
-        }
-      } catch (error) {
-        console.error('Error submitting form:', error);
-      }
-    };
   
-    submitForm();
+  // useEffect(() => {
+  //   const submitForm = async () => {
+  //     const formPurchase = sessionStorage.getItem("formPurchase");
+  //     if (!formPurchase) {
+  //       // console.error("No formPurchase data found in sessionStorage.");
+  //       return; // Exit the function if the data is missing
+  //     }
+  //     try {
+  //       const response = await FetchFunc(
+  //         '/customer-order/create',
+  //         'POST',
+  //         formPurchase
+  //       );
   
-  }, [])
+  //       if (!response.ok) {
+  //         console.log(await response.text()); // Wait for the response text
+  //       } else {
+  //         console.log('Response from server:', await response.json()); // Process response
+  //         sessionStorage.removeItem("formPurchase");
+  //       }
+  //     } catch (error) {
+  //       console.error('Error submitting form:', error);
+  //     }
+  //   };
+  
+  //   submitForm();
+  
+  // }, [])
   
 
   const paymentContext = JSON.parse(sessionStorage.getItem("formPurchase"));
@@ -66,13 +113,16 @@ function Completion(props) {
             <p>Back</p>
           </div>
         </button>
-      <div className="thank_you_text">
-        <h1>Thank You!</h1>
-        <p>A Confirmation has been send to your email! 
-          You can check via your email or our customer portal.</p>
-        <Link to='/' style={{textDecoration: 'none'}}><button className="back_to_home">Back</button></Link>
-      </div>
-      <div id="messages" role="alert" style={messageBody ? {display: 'block'} : {}}>{messageBody}</div>
+      {errorMsg ? 
+        <div className="thank_you_text" id="messages" role="alert" style={messageBody ? {display: 'block'} : {}}><p>{messageBody}</p></div>
+        :
+        <div className="thank_you_text">
+          <h1>Thank You!</h1>
+          <p>A Confirmation has been sent to your email! 
+            You can check via your email or our customer portal.</p>
+          <Link to='/' style={{textDecoration: 'none'}}><button className="back_to_home">Back</button></Link>
+        </div>
+      }
     </div>
   );
 }
--- src/Purchase/Payment.js
+++ src/Purchase/Payment.js
@@ -43,9 +43,9 @@ import "./Payment.css";
 //         </form>
 //     );
 // };
+export const stripePromise = loadStripe("pk_test_51QPcunABwpEZeajXo2pch6nBD11Mj3NckAnEKJIXBa6GVNbByataVG6CuGnlRlfWmoOt9RpHatjU47piXri1d1O700u6TfquCT");
 
 function Payment({ clientSecret }) {
-    const stripePromise = loadStripe("pk_test_51QPcunABwpEZeajXo2pch6nBD11Mj3NckAnEKJIXBa6GVNbByataVG6CuGnlRlfWmoOt9RpHatjU47piXri1d1O700u6TfquCT");
     console.log("payment here:" + clientSecret)
     return (
     <div className='payment_detail'>
